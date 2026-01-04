18600d2990442d7c6520deef07344ca134307574
Santiago Sica <santisica29@gmail.com>
2026-01-04 13:18:29 -0300

    Add project files.

---
+++ HabitTracker/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=habitTracker.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+    var tableCmd = connection.CreateCommand();
+
+    tableCmd.CommandText = 
+        @"CREATE TABLE IF NOT EXISTS coding (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Date TEXT,
+            Quantity INTEGER
+            )";
+
+    tableCmd.ExecuteNonQuery();
+
+    connection.Close();
+}
